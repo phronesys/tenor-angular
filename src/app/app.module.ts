2151798ddafa8a2bc79021eaf4c101e4d178c156
phronesys <daniel.dazarola@gmail.com>
2022-01-08 21:12:18 -0300

    enhance: add logo in app change background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
 import { SvgSearchComponent } from './components/svg/svg-search/svg-search.component';
+import { SvgGifComponent } from './components/svg/svg-gif/svg-gif.component';
+import { SvgGiftComponent } from './components/svg/svg-gift/svg-gift.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    SvgSearchComponent
+    SvgSearchComponent,
+    SvgGifComponent,
+    SvgGiftComponent,
+    LogoComponent,
   ],
   imports: [
     BrowserModule, 
