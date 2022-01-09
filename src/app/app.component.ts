8d79d9166cf7ae3cac3b8a0db102af35cb20dad8
phronesys <daniel.dazarola@gmail.com>
2022-01-09 19:47:25 -0300

    feat: card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './services/http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  response: any;
+  constructor(private httpService: HttpService) { }
+
   inputSearch(value: any) {
     console.log(value);
+  }
+  ngOnInit(): void {
+    this.httpService.getResponse();
   }
 }
