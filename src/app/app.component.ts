16d82858c5a62c02e9f98e978e496045491d31a8
phronesys <daniel.dazarola@gmail.com>
2022-01-14 21:35:00 -0300

    refactor(suggestion): inside input service on input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { Gif } from './services/Gif';
 export class AppComponent implements OnInit {
   keyword: string = "esqueleto dance";
   data: Array<Gif> = [];
-  suggestions: Array<string> = [];
   constructor(private gifService: GifService) { }
 
   /* services methods */
@@ -19,25 +18,10 @@ export class AppComponent implements OnInit {
       .subscribe(data => this.data = data);
   }
 
-  autocomplete = (keyword: string) => {
-    this.gifService
-      .autocomplete(keyword)
-      .subscribe(data => this.suggestions = data)
-  }
-
   /* handle events */
   inputSearch(value: string) {
     this.keyword = value;
     this.search(value);
-  }
-
-  inputAutocomplete(value: string) {
-    this.autocomplete(value)
-  }
-
-  completeSuggested(suggestion: string) {
-    console.log(suggestion);
-    
   }
 
   ngOnInit(): void {
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -7,6 +7,7 @@ import {
   transition,
   // ...
 } from '@angular/animations';
+import { GifService } from '../../services/gif.service';
 
 @Component({
   selector: 'app-input',
@@ -30,21 +31,55 @@ import {
 export class InputComponent implements OnInit {
   @Input() open: boolean = false;
   @Output() onSearch = new EventEmitter<string>();
-  @Output() onKeyup = new EventEmitter<string>();
+  suggestions: Array<string> = [];
   value: string = "";
+  showSuggestions: boolean = true;
 
+  constructor(private gifService: GifService) { }
+
+  /* when user types in */
   modelChange = (value: string) => {
-    this.onKeyup.emit(value);
+    this.debounceAutocomplete(value);
   }
 
+  /* when user clicks on search button */
+  toggle = () => {
+    if (this.open && this.value.length > 0) this.searchValue();
+    this.open = !this.open;
+  }
+
+  /* executed on enter keypress too */
   searchValue = () => {
     this.onSearch.emit(this.value)
     this.value = "";
   }
 
-  toggle = () => {
-    if (this.open && this.value.length > 0) this.searchValue();
-    this.open = !this.open;
+  /* autocomplete gif service */
+  autocomplete = (value: string) => {
+    this.gifService
+      .autocomplete(value)
+      .subscribe(data => {
+        this.suggestions = data
+        this.showSuggestions = true;
+      })
+  }
+
+  /* execute the service with debounce */
+  debounceAutocomplete = (value: string) => {
+    this.autocomplete(value);
+  }
+
+  /* when user clicks on suggestion */
+  updateValue = (suggestion: string) => {
+    this.value = suggestion;
+    this.showSuggestions = false;
+  }
+
+  /* executed on escape keypress */
+  resetInput = () => {
+    this.value = "";
+    this.showSuggestions = false;
+    this.open = false;
   }
 
   ngOnInit(): void {
